5b1732f4cf68b55f4e5ea401bda7fb50cf851e67
arianakc <llccmm118@outlook.com>
2024-07-16 20:29:11 -0700

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,20 @@
+mkdir dependencies
+cd dependencies
+git clone https://github.com/shmsw25/FActScore.git
+git clone https://github.com/sylinrl/TruthfulQA.git
+cd ..
+cd scripts
+mv * ..
+cd ..
+cd src
+mv * ..
+cd ..
+cd dependencies
+mv FActScore ..
+cd ..
+cd data/processed_retrieval_data
+mv * ../..
+cd ../..
+rm -r scripts
+rm -r src
+./setup.sh
